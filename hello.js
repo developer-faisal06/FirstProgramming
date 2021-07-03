589082f8af1b3d9ee910ee478ca00c02c7c4cf4f
developer-faisal06 <faisal.web.06@gmail.com>
2021-07-03 17:13:53 +0600

    first-commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+
+var bananaPrice=12;
+console.log(bananaPrice);
+var kola =18;
+var jam =80;
+var am=50;
+console.log(am+jam)
+
+var price1 = 8;
+var price2= 8;
+var project3 =2;
+
+console.log(price1+price2+project3);
